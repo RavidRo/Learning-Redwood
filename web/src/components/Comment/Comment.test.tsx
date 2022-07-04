f626381f53ba0d2b63949e69ea1a553789f19f85
Ravid Rom <romravid@gmail.com>
2022-07-04 11:06:39 +0300

    Building a Component the Redwood Way

---
+++ web/src/components/Comment/Comment.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@redwoodjs/testing/web'
+
+import Comment from './Comment'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//    https://redwoodjs.com/docs/testing#testing-components
+const COMMENT = {
+  name: 'Rob Cameron',
+  body: 'This is the first comment!',
+  createdAt: '2020-01-01T12:34:56Z',
+}
+
+describe('Comment', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<Comment comment={COMMENT} />)
+    }).not.toThrow()
+  })
+
+  it("Shows comment's details", () => {
+    render(<Comment comment={COMMENT} />)
+
+    expect(screen.getByText(COMMENT.name)).toBeInTheDocument()
+    expect(screen.getByText(COMMENT.body)).toBeInTheDocument()
+    const dateExpect = screen.getByText('2 January 2020')
+    expect(dateExpect).toBeInTheDocument()
+    expect(dateExpect.nodeName).toEqual('TIME')
+    expect(dateExpect).toHaveAttribute('datetime', COMMENT.createdAt)
+  })
+})
